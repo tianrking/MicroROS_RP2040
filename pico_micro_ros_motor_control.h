d1479c3d946cd0acb567055435b24c16a055f780
tianrking <tian.r.king@gmail.com>
2023-10-06 15:59:59 +0800

    Create pico_micro_ros_motor_control.h

---
+++ pico_micro_ros_motor_control.h
@@ -0,0 +1,38 @@
+
+#ifndef PICO_MOTOR_COMTROL_H
+#define PICO_MOTOR_COMTROL_H
+
+#include <stdio.h>
+
+#include "hardware/pio.h"
+#include "hardware/timer.h"
+#include "hardware/pwm.h"
+#include "hardware/adc.h"
+#include "quadrature_encoder.pio.h"
+
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <std_msgs/msg/int32.h>
+#include <std_msgs/msg/float64.h>
+#include <std_msgs/msg/string.h>
+#include <rmw_microros/rmw_microros.h>
+
+#include "pico/stdlib.h"
+#include "pico_uart_transports.h"
+
+float moving_average(float new_value, float window[], int *index);
+int caculate(int now, int target);
+
+uint16_t read_adc(uint channel) ;
+
+void timer_callback(rcl_timer_t *timer, int64_t last_call_time);
+void gpio_2_callback(uint gpio, uint32_t events);
+void ppm_callback(uint gpio, uint32_t events);
+
+void subscription_callback_speed_change(const void *msgin_diy);
+
+#define WINDOW_SIZE 20
+
+#endif /* PICO_MOTOR_COMTROL_H */
